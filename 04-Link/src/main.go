e672d577735438796e2d7b8e359118dd85523050
yang cheng <azerothyangg@gmail.com>
2019-03-23 21:29:55 +0800

    clean

--- 04-Link/src/main.go
+++ 04-Link/src/main.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-	var sum int64 = 0
-	var i int64 = 0
-	for i <= 4000000000 {
-		sum += i
-		i++
-	}
-	fmt.Println(sum)
 	link := linknode.New()
 	link.AddFirst("1")
 	link.AddFirst("2")
@@ -20,6 +13,6 @@ func main() {
 	link.AddFirst("4")
 	link.Add(1, "x")
 	link.AddLast("z")
-	//fmt.Println(link.Node.Next.ToString())
+	fmt.Println(link.Node.ToString())
 	//fmt.Println(222)
 }
